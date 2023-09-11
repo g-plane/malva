f3865b0c3dfa86697442d67c036b7219683cca00
Pig Fang <g-plane@hotmail.com>
2023-09-11 09:51:45 +0800

    simplify

--- malva/src/line_bounds.rs
+++ malva/src/line_bounds.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 pub struct LineBounds(Vec<usize>);
 
 impl LineBounds {
@@ -8,10 +6,10 @@ impl LineBounds {
     }
 
     pub(crate) fn is_away_more_than_one_line(&self, a: usize, b: usize) -> bool {
-        let a = self.0.iter().find_position(|offset| a < **offset);
-        let b = self.0.iter().find_position(|offset| b < **offset);
+        let a = self.0.iter().position(|offset| a < *offset);
+        let b = self.0.iter().position(|offset| b < *offset);
         a.zip(b)
-            .map(|((a, _), (b, _))| if a > b { a - b > 1 } else { b - a > 1 })
+            .map(|(a, b)| if a > b { a - b > 1 } else { b - a > 1 })
             .unwrap_or_default()
     }
 }
