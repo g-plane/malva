8410d1d75c03a5bd954544733c8873d8fae44919
Pig Fang <g-plane@hotmail.com>
2023-09-12 22:44:08 +0800

    refactor calculating line distance

--- malva/src/line_bounds.rs
+++ malva/src/line_bounds.rs
@@ -5,11 +5,14 @@ impl LineBounds {
         Self(memchr::memchr_iter(b'\n', input.as_bytes()).collect())
     }
 
-    pub(crate) fn line_distance(&self, a: usize, b: usize) -> usize {
-        let a = self.0.iter().position(|offset| a < *offset);
-        let b = self.0.iter().position(|offset| b < *offset);
-        a.zip(b)
-            .map(|(a, b)| if a > b { a - b } else { b - a })
+    pub(crate) fn line_distance(&self, start: usize, end: usize) -> usize {
+        debug_assert!(end > start);
+
+        let start = self.0.iter().position(|offset| start < *offset);
+        let end = self.0.iter().position(|offset| end < *offset);
+        start
+            .zip(end)
+            .map(|(start, end)| end - start)
             .unwrap_or_default()
     }
 }
