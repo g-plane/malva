4d4bf08662bc20c6653eed1dfe15878033dc8b5e
Pig Fang <g-plane@hotmail.com>
2023-09-17 18:27:22 +0800

    add dprint support

---
+++ dprint/src/config.rs
@@ -0,0 +1,148 @@
+use dprint_core::configuration::{
+    get_unknown_property_diagnostics, get_value, ConfigKeyMap, ConfigurationDiagnostic,
+    GlobalConfiguration, ResolveConfigurationResult,
+};
+use malva::config::{
+    BlockSelectorLineBreak, FormatOptions, HexCase, LanguageOptions, LayoutOptions, LineBreak,
+    OperatorLineBreak, Quotes,
+};
+
+pub(crate) fn resolve_config(
+    mut config: ConfigKeyMap,
+    global_config: &GlobalConfiguration,
+) -> ResolveConfigurationResult<FormatOptions> {
+    let mut diagnostics = Vec::new();
+    let malva_config = FormatOptions {
+        layout: LayoutOptions {
+            print_width: get_value(
+                &mut config,
+                "printWidth",
+                global_config.line_width.unwrap_or(80),
+                &mut diagnostics,
+            ) as usize,
+            use_tabs: get_value(
+                &mut config,
+                "useTabs",
+                global_config.use_tabs.unwrap_or_default(),
+                &mut diagnostics,
+            ),
+            indent_width: get_value(
+                &mut config,
+                "indentWidth",
+                global_config.indent_width.unwrap_or(2),
+                &mut diagnostics,
+            ) as usize,
+            line_break: match &*get_value(
+                &mut config,
+                "lineBreak",
+                "lf".to_string(),
+                &mut diagnostics,
+            ) {
+                "lf" => LineBreak::Lf,
+                "crlf" => LineBreak::Crlf,
+                _ => {
+                    diagnostics.push(ConfigurationDiagnostic {
+                        property_name: "lineBreak".into(),
+                        message: "invalid value for config `lineBreak`".into(),
+                    });
+                    LineBreak::Lf
+                }
+            },
+        },
+        language: LanguageOptions {
+            hex_case: match &*get_value(
+                &mut config,
+                "hexCase",
+                "lower".to_string(),
+                &mut diagnostics,
+            ) {
+                "ignore" => HexCase::Ignore,
+                "lower" => HexCase::Lower,
+                "upper" => HexCase::Upper,
+                _ => {
+                    diagnostics.push(ConfigurationDiagnostic {
+                        property_name: "hexCase".into(),
+                        message: "invalid value for config `hexCase`".into(),
+                    });
+                    HexCase::Lower
+                }
+            },
+            quotes: match &*get_value(
+                &mut config,
+                "quotes",
+                "alwaysDouble".to_string(),
+                &mut diagnostics,
+            ) {
+                "alwaysDouble" => Quotes::AlwaysDouble,
+                "alwaysSingle" => Quotes::AlwaysSingle,
+                "preferDouble" => Quotes::PreferDouble,
+                "preferSingle" => Quotes::PreferSingle,
+                _ => {
+                    diagnostics.push(ConfigurationDiagnostic {
+                        property_name: "quotes".into(),
+                        message: "invalid value for config `quotes`".into(),
+                    });
+                    Quotes::AlwaysDouble
+                }
+            },
+            operator_linebreak: match &*get_value(
+                &mut config,
+                "operatorLineBreak",
+                "after".to_string(),
+                &mut diagnostics,
+            ) {
+                "before" => OperatorLineBreak::Before,
+                "after" => OperatorLineBreak::After,
+                _ => {
+                    diagnostics.push(ConfigurationDiagnostic {
+                        property_name: "operatorLineBreak".into(),
+                        message: "invalid value for config `operatorLineBreak`".into(),
+                    });
+                    OperatorLineBreak::After
+                }
+            },
+            block_selector_linebreak: match &*get_value(
+                &mut config,
+                "blockSelectorLinebreak",
+                "consistent".to_string(),
+                &mut diagnostics,
+            ) {
+                "always" => BlockSelectorLineBreak::Always,
+                "consistent" => BlockSelectorLineBreak::Consistent,
+                "wrap" => BlockSelectorLineBreak::Wrap,
+                _ => {
+                    diagnostics.push(ConfigurationDiagnostic {
+                        property_name: "blockSelectorLinebreak".into(),
+                        message: "invalid value for config `blockSelectorLinebreak`".into(),
+                    });
+                    BlockSelectorLineBreak::Consistent
+                }
+            },
+            omit_zero_before_dot: get_value(
+                &mut config,
+                "omitZeroBeforeDot",
+                global_config.use_tabs.unwrap_or_default(),
+                &mut diagnostics,
+            ),
+            trailing_comma: get_value(
+                &mut config,
+                "trailingComma",
+                global_config.use_tabs.unwrap_or_default(),
+                &mut diagnostics,
+            ),
+            pad_comments: get_value(
+                &mut config,
+                "padComments",
+                global_config.use_tabs.unwrap_or_default(),
+                &mut diagnostics,
+            ),
+        },
+    };
+
+    diagnostics.extend(get_unknown_property_diagnostics(config));
+
+    ResolveConfigurationResult {
+        config: malva_config,
+        diagnostics,
+    }
+}
---
+++ dprint/src/lib.rs
@@ -0,0 +1,75 @@
+use crate::config::resolve_config;
+use anyhow::{Error, Result};
+#[cfg(target_arch = "wasm32")]
+use dprint_core::generate_plugin_code;
+use dprint_core::{
+    configuration::{ConfigKeyMap, GlobalConfiguration, ResolveConfigurationResult},
+    plugins::{FileMatchingInfo, PluginInfo, SyncPluginHandler, SyncPluginInfo},
+};
+use malva::{config::FormatOptions, format_text, Syntax};
+use std::path::Path;
+
+mod config;
+
+#[cfg(target_arch = "wasm32")]
+type Configuration = FormatOptions;
+
+pub struct MalvaPluginHandler {}
+
+impl SyncPluginHandler<FormatOptions> for MalvaPluginHandler {
+    fn plugin_info(&mut self) -> SyncPluginInfo {
+        SyncPluginInfo {
+            info: PluginInfo {
+                name: env!("CARGO_PKG_NAME").to_string(),
+                version: env!("CARGO_PKG_VERSION").to_string(),
+                config_key: "malva".to_string(),
+                help_url: "".to_string(),
+                config_schema_url: "".to_string(),
+                update_url: None,
+            },
+            file_matching: FileMatchingInfo {
+                file_extensions: vec!["css".into(), "scss".into(), "sass".into(), "less".into()],
+                file_names: vec![],
+            },
+        }
+    }
+
+    fn license_text(&mut self) -> String {
+        include_str!("../../LICENSE").into()
+    }
+
+    fn resolve_config(
+        &mut self,
+        config: ConfigKeyMap,
+        global_config: &GlobalConfiguration,
+    ) -> ResolveConfigurationResult<FormatOptions> {
+        resolve_config(config, global_config)
+    }
+
+    fn format(
+        &mut self,
+        file_path: &Path,
+        file_text: &str,
+        config: &FormatOptions,
+        _: impl FnMut(&Path, String, &ConfigKeyMap) -> Result<Option<String>>,
+    ) -> Result<Option<String>> {
+        let syntax = match file_path.extension().and_then(|s| s.to_str()) {
+            Some(ext) if ext.eq_ignore_ascii_case("css") => Syntax::Css,
+            Some(ext) if ext.eq_ignore_ascii_case("scss") => Syntax::Scss,
+            Some(ext) if ext.eq_ignore_ascii_case("sass") => Syntax::Sass,
+            Some(ext) if ext.eq_ignore_ascii_case("less") => Syntax::Less,
+            _ => {
+                return Err(anyhow::anyhow!(
+                    "unknown file extension of file: {}",
+                    file_path.display()
+                ));
+            }
+        };
+        format_text(file_text, syntax, config)
+            .map(Some)
+            .map_err(Error::from)
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+generate_plugin_code!(MalvaPluginHandler, MalvaPluginHandler {});
