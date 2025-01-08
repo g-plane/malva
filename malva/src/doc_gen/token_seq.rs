f4cd8f577f70efeba020978675edb9438339e281
Pig Fang <g-plane@hotmail.com>
2025-01-08 10:25:31 +0800

    fix: fix extra whitespace in unknown at-rule (fix #23)

--- malva/src/doc_gen/token_seq.rs
+++ malva/src/doc_gen/token_seq.rs
@@ -1,10 +1,6 @@
 use super::DocGen;
 use crate::{ctx::Ctx, state::State};
-use raffia::{
-    ast::*,
-    token::{Token, TokenWithSpan},
-    Spanned,
-};
+use raffia::{ast::*, Spanned};
 use tiny_pretty::Doc;
 
 impl<'s> DocGen<'s> for TokenSeq<'s> {
@@ -17,13 +13,7 @@ impl<'s> DocGen<'s> for TokenSeq<'s> {
             docs.extend(ctx.start_spaced_comments(ctx.get_comments_between(pos, span.start)));
 
             docs.push(token.doc(ctx, state));
-            if let TokenWithSpan {
-                token: Token::Comma(..) | Token::Semicolon(..),
-                ..
-            } = token
-            {
-                docs.push(Doc::soft_line());
-            } else if matches!(iter.peek(), Some(next) if token.span().end < next.span().start) {
+            if matches!(iter.peek(), Some(next) if token.span().end < next.span().start) {
                 docs.push(Doc::soft_line());
             }
 
