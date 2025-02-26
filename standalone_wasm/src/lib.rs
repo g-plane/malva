3e5e6b65d5be905c19050a27841f7fc1fac47fda
Pig Fang <g-plane@hotmail.com>
2025-02-26 10:31:09 +0800

    build: fix cfg

--- standalone_wasm/src/lib.rs
+++ standalone_wasm/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::JsValue;
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen::wasm_bindgen)]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen)]
 pub fn format(input: String, syntax: JsValue, config: JsValue) -> Result<String, JsValue> {
     let syntax = serde_wasm_bindgen::from_value(syntax)?;
     let options = serde_wasm_bindgen::from_value(config)?;
